4c93f5d6dcbfb5f3077e5c39dd479eaabca7f0ba
Bae-Su-Ho <sugh6019@naver.com>
2022-12-09 19:28:32 +0900

    get_countadd char_count

---
+++ test1.py
@@ -0,0 +1,7 @@
+from docx import Document
+
+doc = Document() #문서 개체 생성
+doc.add_heading("문서 자동화 테트 파일") #헤더 추가
+doc.add_paragraph("안녕하세요.") #단락 추가
+doc.save("demo.docx") #파일로 저장
+doc.get_countLetter();
