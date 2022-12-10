df1af8d300e5f06576f9b00043fe630bb6b417e3
Bae-Su-Ho <sugh6019@naver.com>
2022-12-10 16:41:46 +0900

    Co-authored-by: 강현욱 <hyunw9@users.noreply.github.com>

---
+++ docx/eeeeeeee.py
@@ -0,0 +1,9 @@
+from docx import Document
+
+doc = Document() #문서 개체 생성
+doc.add_heading("문서 자동화 테트 파일") #헤더 추가
+doc.add_paragraph("안녕하세요.") #단락 추가
+doc.add_paragraph("hi")
+doc.add_paragraph("hi")
+doc.add_paragraph("hi")
+doc.save("demo22.docx") #파일로 저장
